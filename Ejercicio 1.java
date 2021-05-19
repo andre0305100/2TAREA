02b21f8c13758ac0a20886456e74f6667af3c585
andre0305100 <>
2021-05-19 04:30:55 +0000

    Resolucion

---
+++ Ejercicio 1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Ejercicio01{
+static Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+    Proceso sueldo 46      
+    Definir i, s, q, n como reales
+   i=1
+   s=1500
+   q=6
+   n=1
+   
+   i=1+1
+El sueldo en aumento
+   n=n+1
+   s=s+(s+10/100)
+       s
+       i>q
+El sueldo del sexto año es
+         s
+
+Fin de proceso         
